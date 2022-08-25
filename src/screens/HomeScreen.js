59d86380c5ef18e6df694e724c2106901ebfea07
Alex Kutzke <alex@kutzke.com.br>
2022-08-25 12:22:20 -0300

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Text>HomeScreen!</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+});
+
+export default HomeScreen;
